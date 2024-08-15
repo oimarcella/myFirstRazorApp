1cfba2506a5e9aefbeeaf5ee97bde8989249cf5e
Marcella <marcella.amorimsa@gmail.com>
2024-08-14 21:02:20 -0300

    ViewData, Parametros da pagina, append version para cache, nested css, render section

--- Pages/Categories.cshtml.cs
+++ Pages/Categories.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace RazorApp.Pages;
 public class Categories : PageModel
 {
+    public int Total { get; set; } = 0;
     public List<Category> CategoriesList { get; set; } = new();
     public void OnGet(int pageNumber = 1, int take = 15)
     {// Executado sempre que a página for renderizada, ou seja quando for feito GET
@@ -14,6 +15,7 @@ public class Categories : PageModel
             auxList.Add(new Category(i, $"Categoria-{i}", i * 18.95M));
         }
 
+        Total = auxList.Count;
         var skip = (pageNumber - 1) * take;
         CategoriesList = auxList.Skip(skip).Take(take).ToList();
     }
